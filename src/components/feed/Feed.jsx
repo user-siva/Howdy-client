d85ba4dd809a627e1e71e949eb85bdfd26921569
user-siva <sivag0503@gmail.com>
2022-02-12 14:23:52 +0530

    post ui created

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -8,6 +8,12 @@ function Feed() {
            <div className="feedWrapper">
                <Share />
                <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
+               <Post />
            </div>
         </div>
     );
