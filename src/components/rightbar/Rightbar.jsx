a1951bcc62d54c1c73f0883a8fd208a05edae154
user-siva <sivag0503@gmail.com>
2022-02-13 07:17:47 +0530

    rightbar ui added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,23 @@ import "./rightbar.css"
 function Rightbar() {
     return (
         <div className="rightbar">
-            Rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText"><b>Hattori</b> have birthday today</span>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="onlineFrdList">
+                    <li className="onlineFrd">
+                        <div className="rightbarImgContainer">
+                            <img src="assets/users/8.png" alt="" className="rightbarProfile" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Goku</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
